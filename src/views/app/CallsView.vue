828eaf4e7c0e79c731bde505dfa9753aeab8e13c
Rodrigo Tutz <rodrigo@rodrigotutz.com>
2023-07-04 00:33:29 -0300

    Ajustando layout mobile

--- src/views/app/CallsView.vue
+++ src/views/app/CallsView.vue
@@ -1,9 +1,9 @@
 <template>
-    <main class="container p-5">
-        <h2>Atendimentos</h2>
+    <main class="container pt-5 p-md-5">
+        <h2 class="text-center text-md-start">Atendimentos</h2>
 
-        <div class="mt-5 d-flex justify-content-between align-items-center">
-            <div class="btn btn-light fw-bold"><i class="bi bi-plus-square-fill"></i> Novo Atendimento</div>
+        <div class="mt-5 d-flex justify-content-between align-items-center gap-5">
+            <div class="btn btn-light btn-sm fw-bold"><i class="bi bi-plus-square-fill"></i> Novo Atendimento</div>
             <form>
                 <div class="form-outline">
                     <input type="search" id="search" class="form-control form-control-sm" placeholder="Pesquisar por..." />
@@ -12,7 +12,7 @@
         </div>
 
         <div class="call-not-found d-flex justify-content-center align-items-center">
-            <h2>Não foram encontrados atendimentos</h2>
+            <h2 class="text-center">Não foram encontrados atendimentos</h2>
         </div>
     </main>
 </template>
@@ -21,12 +21,6 @@
 </script>
 
 <style lang="scss" scoped>
-    form {
-        input {
-            width: 250px;
-        }
-    }
-
     .call-not-found {
         min-height: 50vh;
         opacity: 0.2;
