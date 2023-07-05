8c24a63fb2e686262c66545743c0c319a5d0b599
Rodrigo Tutz <rodrigo@rodrigotutz.com>
2023-07-04 22:00:07 -0300

    Criando layout da página administrativa

--- src/views/app/AdminView.vue
+++ src/views/app/AdminView.vue
@@ -1,6 +1,63 @@
 <template>
-  <main class="text-center pt-5">
-    <h1>Página Administrativa</h1>
+  <main class="container pt-5 mt-5">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="input-group mb-3">
+          <select class="form-select" id="">
+            <option selected>Usuários</option>
+            <option value="1">Rodrigo Tutz</option>
+            <option value="2">Marcos Paiva</option>
+            <option value="3">Jeferson Silva</option>
+          </select>
+          <div class="ms-1">
+            <button class="btn btn-light"><i class="bi bi-plus-square-fill"></i></button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="input-group mb-3">
+          <select class="form-select" id="">
+            <option selected>Sistemas</option>
+            <option value="1">SCPI 8</option>
+            <option value="2">SCPI 9</option>
+            <option value="3">SGM</option>
+          </select>
+          <div class="ms-1">
+            <button class="btn btn-light"><i class="bi bi-plus-square-fill"></i></button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="input-group mb-3">
+          <select class="form-select" id="">
+            <option selected>Situações</option>
+            <option value="1">Em Andamento</option>
+            <option value="2">Aguardando Banco</option>
+            <option value="3">Finalizado</option>
+          </select>
+          <div class="ms-1">
+            <button class="btn btn-light"><i class="bi bi-plus-square-fill"></i></button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="input-group mb-3">
+          <select class="form-select" id="">
+            <option selected>Outros</option>
+            <option value="1">Teste</option>
+            <option value="2">Item 2</option>
+            <option value="3">Clientes</option>
+          </select>
+          <div class="ms-1">
+            <button class="btn btn-light"><i class="bi bi-plus-square-fill"></i></button>
+          </div>
+        </div>
+      </div>
+      
+    </div>
   </main>
 </template>
 
