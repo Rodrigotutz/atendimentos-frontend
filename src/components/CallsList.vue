25f3926c1e768bc95dabbc925dd529117279de81
Rodrigo Tutz <rodrigo@rodrigotutz.com>
2023-09-03 11:48:49 -0300

    Ajustando componente JSON

--- src/components/CallsList.vue
+++ src/components/CallsList.vue
@@ -5,7 +5,7 @@
             <NewCall />
             <form>
                 <div class="form-outline">
-                    <input v-model="search" class="form-control form-control-sm shadow fw-bold" placeholder="Pesquisar por.." />
+                    <!--<input v-model="search" class="form-control form-control-sm shadow fw-bold" placeholder="Pesquisar por.." />-->
                 </div>
             </form>
         </div>
@@ -24,13 +24,13 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="call in callsFiltered" :key="call.id">
-                        <th scope="row">{{call.atendimento}}</th>
-                        <td>{{call.relator}}</td>
-                        <td>{{call.sistema}}</td>
-                        <td>{{call.situacao}}</td>
-                        <td>{{call.entidade}}</td>
-                        <td class="case">{{call.caso}}</td>
+                    <tr v-for="call in calls" :key="call.id">
+                        <th scope="row">{{call.at_number}}</th>
+                        <td>{{call.name}}</td>
+                        <td>{{call.system}}</td>
+                        <td>{{call.situation}}</td>
+                        <td>{{call.entity}}</td>
+                        <td class="case">{{call.call_case}}</td>
                         <td class="d-flex flex-row">
                             <button type="button" class="btn btn-danger btn-sm ms-2"><i class="bi bi-trash-fill"></i></button>
                             <button type="button" class="btn btn-primary btn-sm ms-2"><i class="bi bi-pencil-square"></i></button>
@@ -48,37 +48,16 @@
 </template>
 
 <script setup>
-import {ref, computed} from 'vue'
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
 import NewCall from './NewCall.vue';
 
-const search = ref("")
+var calls = ref([])
 
-const calls = ref([
-  { id: 1,atendimento: "105992", relator: "Rodrigo Tutz", sistema: "SCPI 8", situacao: "Finalizado", entidade: "CM Nhandeara", caso: "Cliente relata erro ao empenhar"},
-  { id: 2,atendimento: "105998", relator: "Marcos Paulo", sistema: "SCPI 9", situacao: "Em Andamento", entidade: "PM Votuporanga", caso: "Cliente relata erro ao empenhar"},
-  { id: 3,atendimento: "106992", relator: "César Neto", sistema: "SGM", situacao: "Aguardando Banco", entidade: "CM Mirassol", caso: "Cliente relata erro ao empenhar"},
-  { id: 4,atendimento: "106989", relator: "Rodrigo Antunes", sistema: "SCPI 8", situacao: "Finalizado", entidade: "CM Nhandeara", caso: "Cliente relata erro ao empenhar"},
-  { id: 5,atendimento: "107899", relator: "Marcius Mellem", sistema: "SCPI 9", situacao: "Encaminhado", entidade: "CM Pirapora", caso: "Cliente relata erro ao empenhar"},
-  { id: 6,atendimento: "108987", relator: "Pedro Pinto", sistema: "STS", situacao: "Finalizado", entidade: "PM Bálsamo", caso: "Cliente relata erro ao empenhar"},
-  { id: 7,atendimento: "109959", relator: "Rodrigo Tutz", sistema: "SCPI 8", situacao: "Em Andamento", entidade: "CM Nhandeara", caso: "Cliente relata erro ao empenhar"},
-  { id: 8,atendimento: "104656", relator: "Marcio Silva", sistema: "SCPI 9", situacao: "Finalizado", entidade: "CM Ponta Porãs", caso: "Cliente relata erro ao empenhar"},
-  { id: 9,atendimento: "105989", relator: "Rodrigo Tutz", sistema: "SGM", situacao: "Aguardando Banco", entidade: "PM Theobroma", caso: "Cliente relata erro ao empenhar"},
-  { id: 10,atendimento: "109456", relator: "Marcos Castro", sistema: "SCPI 8", situacao: "Encaminhado", entidade: "PM Mongaguá", caso: "Cliente relata erro ao empenhar"}, 
-]);
-
-const callsFiltered = computed(() => {
-    if(calls.value && search.value) {
-        return calls.value.filter(
-            call => 
-                call.atendimento.includes(search.value) || 
-                call.relator.includes(search.value) || 
-                call.sistema.includes(search.value) || 
-                call.situacao.includes(search.value) || 
-                call.entidade.includes(search.value) || 
-                call.caso.includes(search.value)
-        )
-    }
-    return calls.value
+const fetchCalls = axios.get("https://atendimentos.rodrigotutz.com/api/v1").then((response) => {
+    calls.value = response.data
+}).catch((error) => {
+    console.log(error)
 })
 
 </script>
